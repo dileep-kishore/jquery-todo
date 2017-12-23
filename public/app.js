c9a1bb9af28b83068aca3408543b12b9b342d44a
Dileep Kishore <k.dileep1994@gmail.com>
2017-12-23 09:30:10 -0500

    Fully working TODO app

--- public/app.js
+++ public/app.js
@@ -5,11 +5,22 @@ $(() => {
 
   $('#todoInput').keypress(event => {
     if(event.which == 13) {
-      createTodo();
+      createTodo()
     }
   })
 
   $('#todo-btn').click(createTodo)
+
+  $('.list').on('click', '.icon .fa-times', function(event) {
+    event.stopPropagation()
+    let liObj = $(this).parent().parent()
+    removeTodo(liObj)
+  })
+
+  $('.list').on('click', '.icon .fa-check', function() {
+    let liObj = $(this).parent().parent()
+    checkTodo(liObj)
+  })
 })
 
 function addTodos(todos) {
@@ -22,6 +33,8 @@ function addTodo(todo) {
   let crossIcon = $('<span class="icon"><i class="fa fa-times"></i></span>')
   let newTodo = $('<li><span class="text">' + todo.name + '</span></li>');
   newTodo.addClass("box")
+  newTodo.data('id', todo._id)
+  newTodo.data('completed', todo.completed)
   if(todo.completed) {
     newTodo.addClass("done")
     checkIcon.addClass("has-text-disabled")
@@ -44,4 +57,29 @@ function createTodo() {
       $('#todoInput').val('');
     })
     .catch(err => console.log(err))
+}
+
+function removeTodo(liObj) {
+  // remove todo
+  let liId = liObj.data('id')
+  let deleteUri = '/api/todos/' + liId
+  $.ajax({ method: 'DELETE', url: deleteUri })
+    .then(data => {
+      liObj.remove()
+      console.log(data)
+    })
+    .catch(err => console.log(err))
+}
+
+function checkTodo(liObj) {
+  // check off todo
+  let liId = liObj.data('id')
+  let liCompleted = liObj.data('completed')
+  let updateData = {completed: !liCompleted}
+  let checkUri = '/api/todos/' + liId
+  $.ajax({ method: 'PUT', url: checkUri, data: updateData })
+    .then(data => {
+        liObj.toggleClass('done')
+        liObj.data('completed', !liCompleted)
+    })
 }
